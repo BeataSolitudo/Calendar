f02423ce5832b900ca9915980870c039f7d958ce
BeataSolitudo <j.kalivoda.st@spseiostrava.cz>
2023-05-08 00:00:02 +0200

    Calendar_V0.6

--- main.py
+++ main.py
@@ -3,30 +3,22 @@ from tkinter import ttk
 from tkcalendar import Calendar, DateEntry
 
 
-def set_date_color(date, color):
-    cal.tag_config(color, background=color)
-    cal.calevent_create(date, 'Selected', 'selected_date', tag=color)
-
-
 def on_date_select():
     date = cal.selection_get()
     message_window = tk.Toplevel(root)
     message_window.title('Important message')
-
-    def on_yes_click():
-        set_date_color(date, 'green')
-        message_window.destroy()
-
-    def on_no_click():
-        set_date_color(date, 'red')
-        message_window.destroy()
-
     message_label = ttk.Label(message_window, text='Did you make today something important to you?')
     message_label.pack(padx=20, pady=20)
-    yes_button = ttk.Button(message_window, text='Yes', command=on_yes_click)
+    yes_button = ttk.Button(message_window, text='Yes', command=lambda: on_yes_click(date, message_window))
     yes_button.pack(side=tk.LEFT, padx=20, pady=20)
-    no_button = ttk.Button(message_window, text='No', command=on_no_click)
+    no_button = ttk.Button(message_window, text='No', command=message_window.destroy)
     no_button.pack(side=tk.RIGHT, padx=20, pady=20)
+
+
+def on_yes_click(date, message_window):
+    cal.tag_config('selected_date', background='green')
+    cal.calevent_create(date, 'Selected', 'selected_date')
+    message_window.destroy()
 
 
 root = tk.Tk()
