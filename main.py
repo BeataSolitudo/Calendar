c3c0e06840da4687976187b8fedaf03a74f2dcea
BeataSolitudo <j.kalivoda.st@spseiostrava.cz>
2023-05-07 14:36:06 +0200

    Calendar_V0.5

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ def on_date_select():
     date = cal.selection_get()
     message_window = tk.Toplevel(root)
     message_window.title('Important message')
+
+    def on_yes_click():
+        set_date_color(date, 'green')
+        message_window.destroy()
+
+    def on_no_click():
+        set_date_color(date, 'red')
+        message_window.destroy()
+
     message_label = ttk.Label(message_window, text='Did you make today something important to you?')
     message_label.pack(padx=20, pady=20)
-    yes_button = ttk.Button(message_window, text='Yes', command=lambda: set_date_color(date, 'green'))
+    yes_button = ttk.Button(message_window, text='Yes', command=on_yes_click)
     yes_button.pack(side=tk.LEFT, padx=20, pady=20)
-    no_button = ttk.Button(message_window, text='No', command=lambda: set_date_color(date, 'red'))
+    no_button = ttk.Button(message_window, text='No', command=on_no_click)
     no_button.pack(side=tk.RIGHT, padx=20, pady=20)
 
 
