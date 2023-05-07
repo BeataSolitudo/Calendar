f9429c31d793009e64b3d95f95bd5424bf15ee77
BeataSolitudo <j.kalivoda.st@spseiostrava.cz>
2023-05-07 13:15:24 +0200

    Calendar_V0.0

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Test..")
+import calendar
+
+# print(calendar.weekheader(2)) Get you header of week (Mo Tu We....)
+# print(calendar.firstweekday()) Get you position on what week starts
+# print(calendar.month(2023, 5)) Get you month
+# print(calendar.monthcalendar(2023 ,5)) Get you matrix of month
+# print(calendar.calendar(2023)) Get you pretty months days of year
+
+# day_of_the_week = calendar.weekday(2023, 5, 7) Gets you day of the week
+# print(day_of_the_week)
+
+# is_leap = calendar.isleap(2023) Gets you if the year is leap year
+# print(is_leap)
+
+# how_many_leap_days = calendar.leapdays(2024, 2025) Gets you how many leap days did you skip
+# print(how_many_leap_days)
+
+
+
+
+
+
