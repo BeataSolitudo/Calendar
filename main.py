9c0db6e793db425afa766cf4e7763adf5fd3b61b
BeataSolitudo <j.kalivoda.st@spseiostrava.cz>
2023-05-07 14:22:11 +0200

    Calendar_V0.4

--- main.py
+++ main.py
@@ -4,60 +4,27 @@ from tkcalendar import Calendar, DateEntry
 
 
 def set_date_color(date, color):
-    try:
-        tags = cal.tag_has('selected_date')
-        for tag in tags:
-            cal.tag_remove(tag, '1.0', 'end')
-    except:
-        pass
-    cal.calevent_create(date, 'Selected', 'selected_date', background=color)
+    cal.tag_config(color, background=color)
+    cal.calevent_create(date, 'Selected', 'selected_date', tag=color)
 
 
-def show_message(date):
+def on_date_select():
+    date = cal.selection_get()
     message_window = tk.Toplevel(root)
-    message_window.title('Message')
-    message_window.geometry('300x150')
-
-    message = tk.Label(
-        message_window,
-        text="Did you make today something important to you?"
-    )
-    message.pack(pady=10)
-
-    yes_button = ttk.Button(
-        message_window,
-        text='Yes',
-        command=lambda: set_date_color(date, 'green')
-    )
-    yes_button.pack(pady=10)
-
-    no_button = ttk.Button(
-        message_window,
-        text='No',
-        command=lambda: set_date_color(date, 'red')
-    )
-    no_button.pack(pady=10)
-
-
-def on_date_click(date):
-    show_message(date.strftime('%m/%d/%Y'))
+    message_window.title('Important message')
+    message_label = ttk.Label(message_window, text='Did you make today something important to you?')
+    message_label.pack(padx=20, pady=20)
+    yes_button = ttk.Button(message_window, text='Yes', command=lambda: set_date_color(date, 'green'))
+    yes_button.pack(side=tk.LEFT, padx=20, pady=20)
+    no_button = ttk.Button(message_window, text='No', command=lambda: set_date_color(date, 'red'))
+    no_button.pack(side=tk.RIGHT, padx=20, pady=20)
 
 
 root = tk.Tk()
-root.title('Calendar')
+root.geometry('600x400')
 
-cal = Calendar(
-    root,
-    font=('Arial', 14),
-    selectmode='day',
-    cursor="hand1",
-    year=2023,
-    month=5,
-    day=1,
-)
-cal.pack(fill='both', expand=True)
-
-cal.bind('<<CalendarSelected>>', lambda e: on_date_click(cal.selection_get()))
+cal = Calendar(root, selectmode='day', date_pattern='yyyy-mm-dd')
+cal.pack(fill=tk.BOTH, expand=True)
+cal.bind('<<CalendarSelected>>', lambda event: on_date_select())
 
 root.mainloop()
-
